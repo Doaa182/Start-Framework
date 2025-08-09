9944975f3e4f7b53032db942af768accfaa423ad
x x <20201700240@cis.asu.edu.eg>
2025-08-10 00:45:35 +0300

    Updated Home component and styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,9 @@ import Sharedstar from "../Sharedstar/Sharedstar";
 export default function Home() {
   return (
     <div className="text-white d-flex justify-content-center align-items-center flex-column my-3">
-      <img src={homeImg} alt="Hero Picture" />
+      <img src={homeImg} alt="Hero Picture" style={{ width: "250px" }} />
       <h1 className="text-uppercase fw-bold mt-5">Start Framework</h1>
-      <Sharedstar bgColor={"#ffffff"}  />
+      <Sharedstar bgColor={"#ffffff"} />
       <p>Graphic Artist - Web Designer - Illustrator</p>
     </div>
   );

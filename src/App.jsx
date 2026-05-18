bacc96569abb9e48ace25e0de54feaaa7ce84553
Doaa Diaa El Din <do3a2dia2@gmail.com>
2026-05-18 17:37:54 +0300

    refactor: update Home layout to Islamic theme and fix build issues for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import homeImg from "../../assets/home-img.svg";
+import heroImg from "../../assets/hero-img.png";
 import styles from "./Home.module.css";
 import Sharedstar from "../Sharedstar/Sharedstar";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="text-white d-flex justify-content-center align-items-center flex-column my-3">
-      <img src={homeImg} alt="Hero Picture" style={{ width: "250px" }} />
-      <h1 className="text-uppercase fw-bold mt-5">Start Framework</h1>
+      <img
+        src={heroImg}
+        alt="Hero Picture"
+        className={`${styles["hero-img"]}`}
+      />
+      <h1 className="text-capitalize fw-bold mt-4">Start Framework Islamic</h1>
       <Sharedstar bgColor={"#ffffff"} />
-      <p>Graphic Artist - Web Designer - Illustrator</p>
+      <p className={`${styles["p"]}`}>
+        All-in-one app for Azkar, Salah times, and daily Tasbeeh tracking
+      </p>
+      <Link
+        to="/about"
+        className={`mb-4 text-decoration-none text-white fw-semibold py-2 px-4 ${styles["tasbeeh-btn"]}`}
+      >
+        Start Tasbeeh
+        <FaArrowRight className={`ms-2 ${styles["arrow-right"]}`} />
+      </Link>
     </div>
   );
 }
